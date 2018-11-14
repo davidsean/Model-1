4ff8cf81d233864420544bc8e3a1341bd85085ae
davidsean <david.sean@gmail.com>
2018-11-14 16:03:29 -0500

    Tim's checksum message

--- Arduino Sketches/Main/Incuvers_Incubator/Opt_PiLink.h
+++ Arduino Sketches/Main/Incuvers_Incubator/Opt_PiLink.h
@@ -3,7 +3,11 @@
  * Incuvers PiLink code.
  * 
  * Only functional on 1.0.0+ control board units.
+ * Requires the CRC32 library by Christopher Baker.
  */
+
+#include <CRC32.h>
+
 class IncuversPiLink {
   private:
     IncuversSettingsHandler* incSet;
@@ -14,58 +18,71 @@ class IncuversPiLink {
     }
     
     void SendStatus() {
-      // General Identification
-      Serial1.print(millis());
-      Serial1.print(F(" ID "));              // Identification
-      Serial1.print(incSet->getSerial());
+      // Create string to send
+      String piLink = String(millis(), DEC);
+      piLink = String(piLink + F(" ID "));              // Identification
+      piLink = String(piLink + String(incSet->getSerial()));
+      piLink = String(piLink + F(" IV "));              // Ident Version
+      piLink = String(piLink + F(SOFTWARE_VER_STRING));
       // Heating/Fan system
-      Serial1.print(F(" FM "));              // Fan, mode
-      Serial1.print(incSet->getFanMode());
-      Serial1.print(F(" TM "));              // Temperature, mode
-      Serial1.print(incSet->getHeatMode());
-      Serial1.print(F(" TP "));              // Temperature, setpoint
-      Serial1.print(incSet->getTemperatureSetPoint(), 2);
-      Serial1.print(F(" TC "));              // Temperature, chamber
-      Serial1.print(incSet->getChamberTemperature(), 2);
-      Serial1.print(F(" TD "));              // Temperature, door
-      Serial1.print(incSet->getDoorTemperature(), 2);
-      Serial1.print(F(" TO "));              // Temperature, other
-      Serial1.print(incSet->getOtherTemperature(), 2);
-      Serial1.print(F(" TS "));              // Temperature, status
-      Serial1.print(GetIndicator(incSet->isDoorOn(), incSet->isDoorStepping(), false, true));
-      Serial1.print(GetIndicator(incSet->isChamberOn(), incSet->isChamberStepping(), false, true));
-      Serial1.print(F(" TA "));              // Temperature, alarms
-      Serial1.print(GetIndicator(incSet->isHeatAlarmed(), false, false, true));
+      piLink = String(piLink + F(" FM "));              // Fan, mode
+      piLink = String(piLink + String(incSet->getFanMode(), DEC));
+      piLink = String(piLink + F(" TM "));              // Temperature, mode
+      piLink = String(piLink + String(incSet->getHeatMode(), DEC));
+      piLink = String(piLink + F(" TP "));              // Temperature, setpoint
+      piLink = String(piLink + String(incSet->getTemperatureSetPoint(), 2));
+      piLink = String(piLink + F(" TC "));              // Temperature, chamber
+      piLink = String(piLink + String(incSet->getChamberTemperature(), 2));
+      piLink = String(piLink + F(" TD "));              // Temperature, door
+      piLink = String(piLink + String(incSet->getDoorTemperature(), 2));
+      piLink = String(piLink + F(" TO "));              // Temperature, other
+      piLink = String(piLink + String(incSet->getOtherTemperature(), 2));
+      piLink = String(piLink + F(" TS "));              // Temperature, status
+      piLink = String(piLink + String(GetIndicator(incSet->isDoorOn(), incSet->isDoorStepping(), false, true)));
+      piLink = String(piLink + String(GetIndicator(incSet->isChamberOn(), incSet->isChamberStepping(), false, true)));
+      piLink = String(piLink + F(" TA "));              // Temperature, alarms
+      piLink = String(piLink + String(GetIndicator(incSet->isHeatAlarmed(), false, false, true)));
       // CO2 system
-      Serial1.print(F(" CM "));              // CO2, mode
-      Serial1.print(incSet->getCO2Mode());
-      Serial1.print(F(" CP "));              // CO2, setpoint
-      Serial1.print(incSet->getCO2SetPoint(), 2);
-      Serial1.print(F(" CC "));              // CO2, reading
-      Serial1.print(incSet->getCO2Level(), 2);
-      Serial1.print(F(" CS "));              // CO2, status
-      Serial1.print(GetIndicator(incSet->isCO2Open(), incSet->isCO2Stepping(), false, true));
-      Serial1.print(F(" CA "));              // CO2, alarms
-      Serial1.print(GetIndicator(incSet->isCO2Alarmed(), false, false, true));
+      piLink = String(piLink + F(" CM "));              // CO2, mode
+      piLink = String(piLink + String(incSet->getCO2Mode(), DEC));
+      piLink = String(piLink + F(" CP "));              // CO2, setpoint
+      piLink = String(piLink + String(incSet->getCO2SetPoint(), 2));
+      piLink = String(piLink + F(" CC "));              // CO2, reading
+      piLink = String(piLink + String(incSet->getCO2Level(), 2));
+      piLink = String(piLink + F(" CS "));              // CO2, status
+      piLink = String(piLink + GetIndicator(incSet->isCO2Open(), incSet->isCO2Stepping(), false, true));
+      piLink = String(piLink + F(" CA "));              // CO2, alarms
+      piLink = String(piLink + GetIndicator(incSet->isCO2Alarmed(), false, false, true));
       // O2 system
-      Serial1.print(F(" OM "));              // O2, mode
-      Serial1.print(incSet->getO2Mode());
-      Serial1.print(F(" OP "));              // O2, setpoint
-      Serial1.print(incSet->getO2SetPoint(), 2);
-      Serial1.print(F(" OC "));              // O2, reading
-      Serial1.print(incSet->getO2Level(), 2);
-      Serial1.print(F(" OS "));              // CO2, status
-      Serial1.print(GetIndicator(incSet->isO2Open(), incSet->isO2Stepping(), false, true));
-      Serial1.print(F(" OA "));              // CO2, alarms
-      Serial1.print(GetIndicator(incSet->isO2Alarmed(), false, false, true));
+      piLink = String(piLink + F(" OM "));              // O2, mode
+      piLink = String(piLink + String(incSet->getO2Mode(), DEC));
+      piLink = String(piLink + F(" OP "));              // O2, setpoint
+      piLink = String(piLink + String(incSet->getO2SetPoint(), 2));
+      piLink = String(piLink + F(" OC "));              // O2, reading
+      piLink = String(piLink + String(incSet->getO2Level(), 2));
+      piLink = String(piLink + F(" OS "));              // CO2, status
+      piLink = String(piLink + GetIndicator(incSet->isO2Open(), incSet->isO2Stepping(), false, true));
+      piLink = String(piLink + F(" OA "));              // CO2, alarms
+      piLink = String(piLink + GetIndicator(incSet->isO2Alarmed(), false, false, true));
       // Options
-      Serial1.print(F(" LM "));              // Light Mode
-      Serial1.print(incSet->getLightMode());
-      Serial1.print(F(" LS "));              // Light System
-      Serial1.print(incSet->getLightModule()->GetSerialAPIndicator());
+      piLink = String(piLink + F(" LM "));              // Light Mode
+      piLink = String(piLink + String(incSet->getLightMode(), DEC));
+      piLink = String(piLink + F(" LS "));              // Light System
+      piLink = String(piLink + incSet->getLightModule()->GetSerialAPIndicator());
       // Debugging
-      Serial1.print(F(" FM "));              // Free memory
-      Serial1.print(freeMemory());
+      piLink = String(piLink + F(" FM "));              // Free memory
+      piLink = String(piLink + String(freeMemory(), DEC));
+
+      CRC32 crc;
+      
+      for (int i = 0; i < piLink.length(); i++) {
+        crc.update(piLink[i]);
+      }
+      
+      Serial1.print(piLink);
+      // CRC to detect bad lines
+      Serial1.print(F("||||"));              // Divider
+      Serial1.print(crc.finalize(), HEX);
       Serial1.println();
     }
     
